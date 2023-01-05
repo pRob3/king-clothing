406a520b6f3f8ee00fe20dce3107e701e024cf35
pRob3 <bubblan@slaf.se>
2023-01-05 16:10:46 +0100

    Checkout page and related context

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { CartContext } from '../../contexts/cart.context';
 
 import './cart-dropdown.styles.scss';
@@ -8,6 +9,12 @@ import { CartItem } from '../cart-item/cart-item.component';
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    navigate('/checkout');
+  };
+
   return (
     <div className='cart-dropdown-container'>
       <div className='cart-items'>
@@ -19,7 +26,9 @@ const CartDropdown = () => {
           <span className='empty-message'>Your cart is empty</span>
         )}
       </div>
-      <Button buttonType='inverted'>Go to Checkout</Button>
+      <Button onClick={goToCheckoutHandler} buttonType='inverted'>
+        Go to Checkout
+      </Button>
     </div>
   );
 };
