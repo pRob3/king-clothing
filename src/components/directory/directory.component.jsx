8120005227dda9bad70e58288d9fb13ba8cacf0b
pRob3 <bubblan@slaf.se>
2023-01-06 09:41:26 +0100

    rename

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,11 +1,11 @@
-import CategoryItem from '../category-item/category-item.component';
+import DirtectoryItem from '../directory-item/directory-item.component';
 import './directory.styles.scss';
 
 const Directory = ({ categories }) => {
   return (
     <div className='directory-container'>
       {categories.map((category) => (
-        <CategoryItem key={category.id} category={category} />
+        <DirtectoryItem key={category.id} category={category} />
       ))}
     </div>
   );
