5ce2651b5b8000c3e9112a0c97b04681eaf79863
pRob3 <bubblan@slaf.se>
2023-01-10 18:41:34 +0100

    using reducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -4,6 +4,8 @@ import {
   onAuthStateChangedListener,
 } from '../utils/firebase/firebase.utils';
 
+import { createAction } from '../utils/reducer/reducer.utils';
+
 // Actual value
 export const UserContext = createContext({
   currentUser: null,
@@ -40,10 +42,7 @@ export const UserProvider = ({ children }) => {
   console.log(currentUser);
 
   const setCurrentUser = (user) => {
-    dispatch({
-      type: USER_ACTION_TYPES.SET_CURRENT_USER,
-      payload: user,
-    });
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
   };
 
   const value = { currentUser, setCurrentUser };
