459ddcf5fa700494a787fc95f6a1b1fb747c9726
pRob3 <bubblan@slaf.se>
2023-01-16 05:05:24 +0100

    cleaning and add redux-devtools support

---
+++ src/middleware/logger.js
@@ -0,0 +1,14 @@
+// Own logger middleware (clearer when debugging)
+export const loggerMiddleWare = (store) => (next) => (action) => {
+  if (!action.type) {
+    return next(action);
+  }
+
+  console.log('type: ', action.type);
+  console.log('payload: ', action.payload);
+  console.log('currentState: ', store.getState());
+
+  next(action);
+
+  console.log('nextState: ', store.getState());
+};
--- src/store/store.js
+++ src/store/store.js
@@ -9,21 +9,6 @@ import storage from 'redux-persist/lib/storage';
 
 import { rootReducer } from './root-reducer';
 
-// Own logger middleware (clearer when debugging)
-// const loggerMiddleWare = (store) => (next) => (action) => {
-//   if (!action.type) {
-//     return next(action);
-//   }
-
-//   console.log('type: ', action.type);
-//   console.log('payload: ', action.payload);
-//   console.log('currentState: ', store.getState());
-
-//   next(action);
-
-//   console.log('nextState: ', store.getState());
-// };
-
 const persistConfig = {
   key: 'root',
   storage,
@@ -32,8 +17,16 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const middleWares = [logger];
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(
+  Boolean
+);
+
+const composedEnhancer =
+  (process.env.NODE_ENV !== 'production' &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composedEnhancer(applyMiddleware(...middleWares));
 
 export const store = createStore(
   persistedReducer,
