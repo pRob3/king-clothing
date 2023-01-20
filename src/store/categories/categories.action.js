e3ae74583e5138c02791f80e64e3f262d73a8ec0
pRob3 <bubblan@slaf.se>
2023-01-20 12:25:46 +0100

    fetchCategoriesAsync Thunk to Saga

--- src/store/categories/categories.action.js
+++ src/store/categories/categories.action.js
@@ -1,9 +1,5 @@
 import CATEGORIES_ACTION_TYPES from './categories.types';
 import { createAction } from '../../utils/reducer/reducer.utils';
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
-
-export const setCategories = (categoriesArray) =>
-  createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesArray);
 
 export const fetchCategoriesStart = () =>
   createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
@@ -16,14 +12,3 @@ export const fetchCategoriesSuccess = (categoriesArray) =>
 
 export const fetchCategoriesFail = (error) =>
   createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAIL, error);
-
-export const fetchCategoriesAsync = () => async (dispatch) => {
-  dispatch(fetchCategoriesStart());
-
-  try {
-    const catergoriesArray = await getCategoriesAndDocuments('categories');
-    dispatch(fetchCategoriesSuccess(catergoriesArray));
-  } catch (error) {
-    dispatch(fetchCategoriesFail(error));
-  }
-};
